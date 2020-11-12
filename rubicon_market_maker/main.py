64f252b133d6e387797407f5346e74147479ac07
unknown <benjamin.hughes@utexas.edu>
2020-11-12 16:51:50 -0600

    setup and constructor

--- rubicon_market_maker/main.py
+++ rubicon_market_maker/main.py
@@ -1,5 +1,9 @@
 import rubicon_market_maker.marketmaker as mm
 
 def main():
-    x = mm.MarketMaker()
+    pair = "WAYNE/DAI" #Expressed as a string with a slash seperating the assets
+    spread = 0.01 #Expressed as a decimal/percentage
+    refresh_rate = 10 #Quantity of seconds before a refresh of market making trades
 
+    #Initialize Market Making bot
+    trading_bot = mm.MarketMaker(pair, spread, refresh_rate)
--- rubicon_market_maker/marketmaker.py
+++ rubicon_market_maker/marketmaker.py
@@ -1,9 +1,20 @@
+from web3 import Web3
+import json
+import time
+
 # Goal is to have MarketMaker represent the core object and logic of the package
 #MarketMaker should have a "run" function in order to start the bot live on-chain based on a few inputs
 
 class MarketMaker:
-    def test():
-        print("Test function")
-    def __init__(self):
-        self.data = []
-        print('hello world')
+    # Constructor which takes a pair, spread, and refresh_rate
+    def __init__(self, pair, spread, refresh_rate):
+        #Map inputs to Class Variables
+        self.pair = pair.split('/')
+        self.baseAsset = self.pair[0]
+        self.quoteAsset = self.pair[1]
+        self.spread = spread
+        self.refresh_rate = refresh_rate
+
+        #Connect to Web3 provider
+
+        print('***Bot Initialization Complete***\n')
